1cd6156d4510bbaa50668b353e495637b77c93d5
unknown <charayaamit@gmail.com>
2022-03-17 12:32:45 +0530

    encryption added

--- cbdtapp/models.py
+++ cbdtapp/models.py
@@ -2,28 +2,28 @@ from django.db import models
 from django_cryptography.fields import encrypt
 # Create your models here.
 class Header(models.Model):
-    filename=models.CharField(max_length=100)
-    headerIdentifier=models.CharField(max_length=2)
-    orignatorCode=models.CharField(max_length=11)
-    responderCode=models.CharField(max_length=11)
-    fileRefernceNo=models.CharField(max_length=10)
-    totalNoOfRecords=models.CharField(max_length=6)
+    filename=encrypt(models.CharField(max_length=100))
+    headerIdentifier=encrypt(models.CharField(max_length=2))
+    orignatorCode=encrypt(models.CharField(max_length=11))
+    responderCode=encrypt(models.CharField(max_length=11))
+    fileRefernceNo=encrypt(models.CharField(max_length=10))
+    totalNoOfRecords=encrypt(models.CharField(max_length=6))
     def __str__(self):
         return self.filename
 
 class Records(models.Model):
     header=models.ForeignKey(Header,on_delete=models.CASCADE)
-    recordIdentifer=models.CharField(max_length=2)
-    recordRefernceNo=models.CharField(max_length=15)
-    ifscCode=models.CharField(max_length=11)
+    recordIdentifer=encrypt(models.CharField(max_length=2))
+    recordRefernceNo=encrypt(models.CharField(max_length=15))
+    ifscCode=encrypt(models.CharField(max_length=11))
     destinationBankAccountNo=encrypt(models.CharField(max_length=35))
-    accountValidFlag=models.CharField(max_length=2)
-    jointAccountFlag=models.CharField(max_length=2)
+    accountValidFlag=encrypt(models.CharField(max_length=2))
+    jointAccountFlag=encrypt(models.CharField(max_length=2))
     primaryPan=encrypt(models.CharField(max_length=10))
     secondaryPan=encrypt(models.CharField(max_length=10))
-    primaryAccountHolderName=models.CharField(max_length=50)
-    secondaryAccountHolderName=models.CharField(max_length=50)
-    accountType=models.CharField(max_length=2)
+    primaryAccountHolderName=encrypt(models.CharField(max_length=50))
+    secondaryAccountHolderName=encrypt(models.CharField(max_length=50))
+    accountType=encrypt(models.CharField(max_length=2))
 
     def __str__(self):
         return self.header.filename + " " +self.recordRefernceNo
